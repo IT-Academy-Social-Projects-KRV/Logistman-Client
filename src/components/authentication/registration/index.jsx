a4d4982cf6daec62f99a15e60378b4e6e2c4457b
Maryna Kernychna <marynakernychna@gmail.com>
2022-04-15 10:24:05 +0300

    Edit registration page layout
    > Add an adaptation to the registration page
    > Add links to the Login and Home pages
    > Add authentication service
    > Add a general style file

--- src/components/authentication/registration/index.jsx
+++ src/components/authentication/registration/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import styles from '../styles.module.css';
 import { Form, Input, Button } from 'antd';
+import { register } from '../../../services/authentication';
+import { Link } from 'react-router-dom';
 
 function Registration() {
+
+    const onFinish = (values) => {
+        register(values);
+    };
+
+    const onFinishFailed = () => {
+        //trigger the error alert (?)
+    };
 
     return (
         <div className={styles.body}>
@@ -10,13 +20,15 @@ function Registration() {
                 <p className={styles.title}>Logistman</p>
                 <p>Sign up and start managing your offers!</p>
 
-                <Form
+                <Form //needs to add data validation 
                     name="basic"
                     labelCol={{ span: 8 }}
                     wrapperCol={{ span: 16 }}
                     initialValues={{ remember: true }}
                     autoComplete="off"
                     className={styles.form}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
                 >
                     <Form.Item
                         className={styles.textInput}
@@ -59,6 +71,16 @@ function Registration() {
                         </Button>
                     </Form.Item>
                 </Form>
+
+                <div className={styles.linkDiv}>
+                    <Link to="/">
+                        Home
+                    </Link>
+                    <Link to="/login">
+                        Login
+                    </Link>
+                </div>
+
             </div>
         </div>
     );
---
+++ src/services/authentication.js
@@ -0,0 +1,4 @@
+export function register(values) {
+    // form request model from the values
+    // send a request to the server and process the response
+}
