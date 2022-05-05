7ac7ae783182adb3771ec46f49bda475942abf1b
Maryna Kernychna <marynakernychna@gmail.com>
2022-05-05 19:20:45 +0300

    Rewrite to functional component

--- src/components/userProfile/index.jsx
+++ src/components/userProfile/index.jsx
@@ -1,86 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { getUserProfileInfo } from '../../services/userService';
 import Header from '../navigation/header';
 import { Input, Layout } from 'antd';
 import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
 
-class UserProfilePage extends React.Component {
+function UserProfilePage() {
 
-    state = {
-        profileInfo: {},
-        fullName: ""
-    }
+    const [userData, setUserData] = useState({});
+    const [fullName, setFullName] = useState("Undefined");
 
-    async componentDidMount() {
-        this.setState({
-            profileInfo: await getUserProfileInfo()
-        }, () => {
-            this.setState({
-                fullName: this.state.profileInfo.name + ' ' + this.state.profileInfo.surname
-            });
-        });
-    }
+    useEffect(async () => {
+        setUserData(await getUserProfileInfo());
+        setFullName(userData.name + ' ' + userData.surname);
+    }, []);
 
-    render() {
-        const { profileInfo } = this.state;
+    return (
+        <Layout className="profilePageBody" >
+            <Header />
 
-        return (
-            <Layout className="profilePageBody" >
-                <Header />
-
-                <Layout id="infoBlock">
-                    <div className="info">
-                        <div className="infoName">
-                            <p> Full name </p>
-                        </div>
-                        <div className="infoInput">
-                            <Input value={this.state.fullName} />
-                        </div>
+            <Layout id="infoBlock">
+                <div className="info">
+                    <div className="infoName">
+                        <p> Full name </p>
                     </div>
-
-                    <div className="info">
-                        <div className="infoName">
-                            <p> Name </p>
-                        </div>
-                        <div className="infoInput">
-                            <Input value={profileInfo.name} />
-                        </div>
+                    <div className="infoInput">
+                        <Input value={userData.fullName} />
                     </div>
+                </div>
 
-                    <div className="info">
-                        <div className="infoName">
-                            <p> Surname </p>
-                        </div>
-                        <div className="infoInput">
-                            <Input value={this.state.profileInfo.surname} />
-                        </div>
+                <div className="info">
+                    <div className="infoName">
+                        <p> Name </p>
                     </div>
-
-                    <div className="info">
-                        <div className="infoName">
-                            <p> Email </p>
-                        </div>
-                        <div className="infoInput">
-                            <Input value={profileInfo.email} />
-                        </div>
+                    <div className="infoInput">
+                        <Input value={userData.name} />
                     </div>
+                </div>
 
-                    <div className="info">
-                        <div className="infoName">
-                            <p> Is your email confirmed </p>
-                        </div>
-                        <div className="infoInput">
-                            {profileInfo.isEmailConfirmed ?
-                                <CheckOutlined /> :
-                                <CloseOutlined />
-                            }
-                        </div>
+                <div className="info">
+                    <div className="infoName">
+                        <p> Surname </p>
                     </div>
-                </Layout>
+                    <div className="infoInput">
+                        <Input value={userData.surname} />
+                    </div>
+                </div>
 
-            </Layout >
-        );
-    }
+                <div className="info">
+                    <div className="infoName">
+                        <p> Email </p>
+                    </div>
+                    <div className="infoInput">
+                        <Input value={fullName.email} />
+                    </div>
+                </div>
+
+                <div className="info">
+                    <div className="infoName">
+                        <p> Is your email confirmed </p>
+                    </div>
+                    <div className="infoInput">
+                        {userData.isEmailConfirmed ?
+                            <CheckOutlined /> :
+                            <CloseOutlined />
+                        }
+                    </div>
+                </div>
+            </Layout>
+
+        </Layout >
+    );
 }
 
 export default UserProfilePage;
