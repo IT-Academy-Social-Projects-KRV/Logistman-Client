bccbc67e989c34320d505436c079af35e9bb028d
Maryna Kernychna <marynakernychna@gmail.com>
2022-05-11 23:03:11 +0300

    Add work with user's role
    > Add comments
    > Add work with user's role

--- src/reduxActions/auth/index.js
+++ src/reduxActions/auth/index.js
@@ -6,8 +6,9 @@ export const logout = () => {
     };
 }
 
-export const setUserRole = () => {
+export const setAccess = (token) => {
     return {
-        type: types.SET_USER_ROLE
+        type: types.SET_ACCESS,
+        payload: token
     };
 }
