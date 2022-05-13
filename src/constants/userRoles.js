9f5caf85902a3f98a224edef03113a13f489ecc9
Maryna Kernychna <marynakernychna@gmail.com>
2022-05-13 11:13:08 +0300

    Change comment

--- src/constants/userRoles.js
+++ src/constants/userRoles.js
@@ -1,4 +1,4 @@
 export const userRoles = {
     GUEST: 'Guest',
-    USER: 'User'    // when adding a role, add it to authReducer in the set user role case
+    USER: 'User'    // when adding a role, add it to authReducer in the set access case
 }
