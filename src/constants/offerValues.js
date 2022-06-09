fc2ca24a0238956ed186b54400d2c8b56a233958
Vadym <chorrny228@gmail.com>
2022-06-09 09:08:17 +0300

    change default coordinates

--- src/constants/offerValues.js
+++ src/constants/offerValues.js
@@ -1,6 +1,6 @@
 export const offerValues = {
     ORDER_BY_DEFAULT: 1,
     MIN_HOURS_VALUE: 12,
-    DEFAULT_LAT_VALUE: 50.64,
-    DEFAULT_LNG_VALUE: 26.26
+    DEFAULT_LAT_VALUE: 49.84,
+    DEFAULT_LNG_VALUE: 24.03
 }
