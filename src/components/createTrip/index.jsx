a36ca2e727f5fadc25c5bd0866effb14808ed142
Andrii <andrewchepeliuk@gmail.com>
2022-06-19 20:15:20 +0300

    Added map

--- src/components/createTrip/index.jsx
+++ src/components/createTrip/index.jsx
@@ -1,24 +1,92 @@
-import React, {useEffect} from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import {Form, Input, Button, DatePicker, Select} from "antd";
+import Geocode from "react-geocode";
 import Header from "../navigation/header";
-import {GoogleMap} from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { offerValues } from "../../constants/offerValues";
+
+Geocode.setApiKey(process.env.REACT_APP_API_KEY);
+
+const containerStyle = {
+    width: "100%",
+    height: "100%",
+};
+
+const center = {
+    lat: offerValues.DEFAULT_LAT_VALUE,
+    lng: offerValues.DEFAULT_LNG_VALUE,
+};
+
+const defaultOptions = {
+    panControl: true,
+    zoomControl: true,
+    zoomEnabled: true,
+    mapTypeControl: true,
+    disableDefaultUI: true,
+    streetViewControl: true,
+    rotateControl: true,
+    clickableIcons: true,
+    keyboardShortcuts: true,
+    fullscreenControl: true,
+};
 
 function AddOfferToTripPage() {
 
-    return (
+    const { isLoaded } = useJsApiLoader({
+        id: "google-map-script",
+        googleMapsApiKey: process.env.REACT_APP_API_KEY,
+    });
+
+    const [map, setMap] = useState();
+    const [clickedLatLng, setClickedLatLng] = useState(center);
+
+    const onLoad = useCallback(function callback(map) {
+        const bounds = new window.google.maps.LatLngBounds(center);
+        map.fitBounds(bounds);
+        setMap(map);
+    }, []);
+
+    const onUnmount = useCallback(function callback(map) {
+        setMap(null);
+    }, []);
+
+    return isLoaded ? (
         <>
-            <Header/>
-            <div className={"createTripBody"}>
-                <div><h1>Create Trip</h1></div>
-                <div className={"component"}>
+            <Header />
+            <div className="createTripBody">
+                <p className="title">Create trip</p>
+                <div className="mapBlok_addOfferToTrip">
                     <GoogleMap
+                        mapContainerStyle={containerStyle}
+                        center={clickedLatLng}
+                        onLoad={onLoad}
+                        onUnmount={onUnmount}
+                        options={defaultOptions}
+                        id="map"
                     >
                     </GoogleMap>
                 </div>
-                <div className={"component-block"}>
-                    <div className={"component"}><p>Offers</p></div>
-                    <div className={"component"}><p>InfoTrip</p></div>
+                <div className="component-block">
+                    <div className="offersComponent">
+                        <p>Offers</p>
+                    </div>
+                    <div className="routeComponent">
+                        <p>InfoTrip</p>
+                    </div>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        className="submitButton"
+                    >
+                        Create trip
+                    </Button>
                 </div>
+
             </div>
+        </>
+    ) : (
+        <>
+            <span>Map is not loaded!</span>
         </>
     );
 }
