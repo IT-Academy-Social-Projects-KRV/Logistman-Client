c0540d26a984da673cfc76981a2147085a1db07b
Grasmus <91031013+Grasmus@users.noreply.github.com>
2022-07-16 20:20:17 +0300

    Updated messages

--- src/constants/messages/inputValidationErrors.js
+++ src/constants/messages/inputValidationErrors.js
@@ -1,31 +1,31 @@
 export const inputValidationErrorMessages = Object.freeze({
-    NAME_MUST_BE_BETWEEN_1_AND_50: "The name must be between 1 and 50 letters!",
-    SURNAME_MUST_BE_BETWEEN_1_AND_50: "The surname must be between 1 and 50 letters!",
+    NAME_MUST_BE_BETWEEN_1_AND_50: "The length of the name must be between 1 and 50 letters!",
+    SURNAME_MUST_BE_BETWEEN_1_AND_50: "The length of the surname must be between 1 and 50 letters!",
     DESCRIPTION_MUST_BE_BETWEEN_1_AND_1000: "The description must be between 1 and 1000 characters!",
 
-    EMPTY_NAME: "Please enter your name!",
-    EMPTY_SURNAME: "Please enter your surname!",
-    EMPTY_EMAIL: "Please enter your e-mail!",
-    EMPTY_PASSWORD: "Please enter your password!",
-    EMPTY_ORIGIN_ADDRESS: "Please select the origin address!",
-    EMPTY_DESTINATION_ADDRESS: "Please select the destination address!",
-    EMPTY_DESCRIPTION: "Please enter the trip description!",
+    EMPTY_NAME: "Please, enter your name!",
+    EMPTY_SURNAME: "Please, enter your surname!",
+    EMPTY_EMAIL: "Please, enter your e-mail!",
+    EMPTY_PASSWORD: "Please, enter your password!",
+    EMPTY_ORIGIN_ADDRESS: "Please, select the origin address!",
+    EMPTY_DESTINATION_ADDRESS: "Please, select the destination address!",
+    EMPTY_DESCRIPTION: "Please, enter the trip description!",
 
-    EMPTY_USER_NAME: "Please enter user name!",
-    EMPTY_USER_SURNAME: "Please enter user surname!",
-    EMPTY_USER_EMAIL: "Please enter user e-mail!",
+    EMPTY_USER_NAME: "Please, enter user name!",
+    EMPTY_USER_SURNAME: "Please, enter user surname!",
+    EMPTY_USER_EMAIL: "Please, enter user e-mail!",
 
     NOT_VALID_NAME: "The name must start with a capital letter and " +
-        "continue with lowercase letters!\nAnd only latin letters!",
+        "continue with lowercase letters!\nOnly Latin letters are allowed!",
     NOT_VALID_SURNAME: "The surname must start with a capital letter " +
-        "and continue with lowercase letters!\nAnd only latin letters!",
+        "and continue with lowercase letters!\nOnly Latin letters are allowed!",
     NOT_VALID_EMAIL: "Invalid e-mail entered!",
     NOT_VALID_PASSWORD: "Password must contain one or more uppercase " +
         "and lowercase letters, one or more digits and special characters!\n" +
         "The password must be between 8 and 50 symbols!",
 
-    CONFIRM_PASSWORD: "Please confirm your password!",
-    PASSWORD_DOESNT_MATCH: "The two passwords that you entered does not match!",
+    CONFIRM_PASSWORD: "Please, confirm your password!",
+    PASSWORD_DOESNT_MATCH: "The two passwords that you entered do not match!",
 
-    UNSELECTED_DATE: "Choose a date!"
+    UNSELECTED_DATE: "Please, choose a date!"
 });
--- src/constants/messages/offers.js
+++ src/constants/messages/offers.js
@@ -1,9 +1,8 @@
 export const offersMessages = Object.freeze ({
-    LOAD_USER_OFFERS_FAILED: "Load user's offers failed!",
+    LOAD_USER_OFFERS_FAILED: "Failed to load user's offers!",
     CREATE_OFFER_SUCCESS: "Offer successfully created!",
-    CREATE_OFFER_FAILED: "Failed  to create an offer!",
-    TIME_INTERVAL_INCORRECT: "Time interval must be minimum 12 hours between start date and expiration date!",
-    NOT_VALID_GOOD_WEIGHT_MESSAGE: "Negative values cannot be entered in this field!",
+    CREATE_OFFER_FAILED: "Failed to create an offer!",
+    NOT_VALID_GOOD_WEIGHT_MESSAGE: "Negative values are not allowed!",
     DELETE_OFFER_FAILED: "Failed to delete the offer!",
 
     EMPTY_FIELD: "The field cannot be empty!"
