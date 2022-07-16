ee98d5296f4c295b23c5ba0d7f547d287cc90506
Andrii <andrewchepeliuk@gmail.com>
2022-07-17 00:21:42 +0300

    Changed CreateTrip Page

--- src/components/myRoutes/myRoute/index.jsx
+++ src/components/myRoutes/myRoute/index.jsx
@@ -64,7 +64,7 @@ function MyRoute(props) {
                                 <div className="dates">
                                     <div className="date">
                                         <FiCalendar size={DEFAULT_ICON_SIZE} />
-                                        {moment(props.data.startDate).format('LLL') + " "}
+                                        {moment(props.data.departureDate).format('LLL') + " "}
                                     </div>
 
                                     <div className="date">
