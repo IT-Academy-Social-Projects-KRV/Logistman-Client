5188a04ddd1dfcf8d4d8707c283215ae8de9f85b
Eugen <56602775+Eugene-pas@users.noreply.github.com>
2022-07-18 13:56:48 +0300

    finish write tripInfo style

--- src/components/confirmGoodsDeliveryForDriver/index.jsx
+++ src/components/confirmGoodsDeliveryForDriver/index.jsx
@@ -1,27 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import {getTripInfo} from "../../services/trips";
 import Header from "../navigation/header";
 import TripInfo from "./infoTrip";
 
 function ConfirmGoodsDeliveryForDriver() {
-    const[tripInfo,setTripInfo] = useState();
+    const [tripInfo, setTripInfo] = useState();
 
     useEffect(() => {
-       async function fetchData(){
-           setTripInfo(await getTripInfo(1));
-       }
+        async function fetchData() {
+            setTripInfo(await getTripInfo(1));
+        }
 
         fetchData();
-    },[])
+    }, [])
 
     console.log("data", tripInfo);
 
-    return(
+    return (
         <div>
-            <Header />
+            <Header/>
 
-            <div className="confirmTripOfferBody">
-                <TripInfo tripInfo={tripInfo}/>
+            <div className="confirmGoodsDeliveryForDriverPage">
+                <h1 className="titel">Active Trip</h1>
+
+                <div className="confirmTripOfferBody">
+                    <TripInfo tripInfo={tripInfo}/>
+                </div>
             </div>
         </div>
     );
